719c70cfacb108912802360855e229d2bfe1a963
DineshTechWhiz <dineshthiyagarajan337@gmail.com>
2024-07-04 13:25:40 +0530

    New Test

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -41,14 +41,14 @@ const MyWork = () => {
 								<a href="#" class="btn btn-primary">Go somewhere</a>
 							</div>
 						</div>
-						<div class="card text-center col-sm-6">
-							{/* <img src={B1} class="card-img-top" alt="..." /> */}
+						{/* <div class="card text-center col-sm-6">
+							<img src={B1} class="card-img-top" alt="..." />
 							<div class="card-body">
 								<h5 class="card-title">Card title</h5>
 								<p class="card-text">Some quic example text to build on the card title and make up the bulk of the card's content.</p>
 								<a href="#" class="btn btn-primary">Go somewhere</a>
 							</div>
-						</div>
+						</div> */}
 					</div>
 				</div>
 			</section>
